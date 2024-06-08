c91395b6aecc9ae5613a859f6be55022ebe604c2
Sebastian Mestre <mestresebastianunr@gmail.com>
2024-06-08 12:28:27 -0300

    add basic multi-line evaluation

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
 
-const env = new Map;
-env.set('x', Value.Vector());
+const inputs = [];
 
-document.getElementById('codess').addEventListener('input', evt => {
-	const ast = parse(evt.target.value);
-	console.log(ast);
-	const value = eval_expr(env, ast);
-	console.log(value);
+function add_input() {
+	const div = document.createElement('div');
+	const input = document.createElement('input');
+	const label = document.createElement('span');
+	label.innerHTML = '-';
+	div.appendChild(input);
+	div.appendChild(label);
+	inputs.push({input, label});
+	input.addEventListener('input', evt => { compute() });
+	document.body.appendChild(div);
+}
+
+function compute() {
+
+	const env = new Map;
+
+	env.set('x', Value.Vector());
+
 	console.log(env);
-	document.getElementById('codessresult').innerHTML = show(value);
-});
+
+	for (const {input, label} of inputs) {
+		const ast = parse(input.value);
+		console.log(ast);
+		const value = eval_expr(env, ast);
+		console.log(value);
+		console.log(env);
+		label.innerHTML = show(value);
+	}
+
+}
+
+add_input();
+add_input();
+add_input();
